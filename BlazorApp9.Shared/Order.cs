b0b318ceaeabbfaf73757258591f6f06406e2979
markus <modj62361@edu.ucl.dk>
2025-03-23 03:00:39 +0100

    shared added

---
+++ BlazorApp9.Shared/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp9.Shared
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
